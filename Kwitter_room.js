3ab600130d505ef2132032dd1a57bac3b03d771d
The-coder-of-websites <89913566+The-coder-of-websites@users.noreply.github.com>
2022-01-22 16:35:24 +0530

    Add files via upload

---
+++ Kwitter_room.js
@@ -0,0 +1,11 @@
+var firebaseConfig = {
+    apiKey: "AIzaSyDpFCVYijn--2EW192ZzEdWMuHWlSTxiko",
+    authDomain: "kwitter-project-61b83.firebaseapp.com",
+    databaseURL: "https://kwitter-project-61b83-default-rtdb.firebaseio.com",
+    projectId: "kwitter-project-61b83",
+    storageBucket: "kwitter-project-61b83.appspot.com",
+    messagingSenderId: "688101635919",
+    appId: "1:688101635919:web:ec6f602de89019c90e373a"
+  };
+
+  firebase.initializeApp(firebaseConfig);
